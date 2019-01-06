ac2e8205fef4e6003af4103922add276e245d831
Faiz <faiz.ggmu@gmail.com>
2019-01-06 15:42:56 +0500

    Routing Excercise

--- Flask Basics/05-Routing_Exercise.py
+++ Flask Basics/05-Routing_Exercise.py
@@ -1,22 +1,31 @@
 # Set up your imports here!
 # import ...
+from flask import Flask
+app = Flask(__name__)
 
 
-@app.route('') # Fill this in!
+@app.route('/') # Fill this in!
 def index():
     # Welcome Page
     # Create a generic welcome page.
-    pass
+    return '<h1> Welcome to latin Puppy page </h1>'
+    
 
-@app.route('') # Fill this in!
+@app.route('/puppy_latin/<name>') # Fill this in!
 def puppylatin(name):
     # This function will take in the name passed
     # and then use "puppy-latin" to convert it!
-
+    pupname = ''
+    if name[-1] =='y':
+        pupname = name[:-1]+'iful'
+        print(pupname)
+    else:
+        pupname = name + 'y'
+    return "<h1>Hi {}! Your puppylatin name is {} </h1>".format(name,pupname)
     # HINT: Use indexing and concatenation of strings
     # For Example: "hello"+" world" --> "hello world"
-    pass
+    
 
 if __name__ == '__main__':
     # Fill me in!
-    pass
+    app.run()
